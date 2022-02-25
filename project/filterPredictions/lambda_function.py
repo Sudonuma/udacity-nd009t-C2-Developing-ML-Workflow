d3df0c680d52fca59be6dcc2469b6a059d591783
Sudonuma <tasnim.hd@gmail.com>
2022-02-25 10:46:29 +0000

    fix filter lambda function

--- project/filterPredictions/lambda_function.py
+++ project/filterPredictions/lambda_function.py
@@ -3,19 +3,13 @@ THRESHOLD = .94
 
 def lambda_handler(event, context):
    # Grab the inferences from the event
-    
-    inference = event["body"]["inferences"]
 
+    
+    body = json.loads(event['body'])
+    inference = body['inferences']
+    
     meets_threshold = inference[0] > THRESHOLD or inference[1] > THRESHOLD
     
-    # if max(inferences) > THRESHOLD:
-    #     meets_threshold = True 
-    # else:
-    #     meets_threshold = False
-    # print(meets_threshold)
-# If our threshold is met, pass our data back out of the
-# Step Function, else, end the Step Function with an error
-    # meets_threshold = False
     if meets_threshold == True:
         pass
     else:
